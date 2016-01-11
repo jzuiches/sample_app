9a7279e72023d2d23088a98fe053a82e14a53840
Joe Zuiches <jzuiches@yahoo.com>
2016-01-10 20:50:25 -0800

    change title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Squaw Valley Patrol Training App"
+  end
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Squaw Valley Patrol Training App"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Squaw Valley Patrol Training App"
+    assert_select "title", "Help | #{@base_title}"
 
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Squaw Valley Patrol Training App"
+    assert_select "title", "About | #{@base_title}"
 
   end
 
