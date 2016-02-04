360cb2e102c3f9f2360f882b0f357d66eddbacf9
Joe Zuiches <jzuiches@yahoo.com>
2016-02-04 05:40:48 -0800

    add one training division test

--- test/models/possible_training_division_test.rb
+++ test/models/possible_training_division_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class PossibleTrainingDivisionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @possible = PossibleTrainingDivision.new(user_id: 1, training_division_id: 1)
+  end
+
+  test "should be valid" do
+    assert @possible.valid?
+  end
 end
