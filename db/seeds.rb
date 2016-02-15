50ed5fc1347ce6a0dc8572771765d8c40aedcccd
Joe Zuiches <jzuiches@yahoo.com>
2016-02-14 20:00:11 -0800

    add seeds for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-20.times do |n|
-  name  = Faker::Name.name
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(name: name, email: email, password: password, password_confirmation: password,activated: true, activated_at: Time.zone.now)
-end
+# 20.times do |n|
+#   name  = Faker::Name.name
+#   email = Faker::Internet.email
+#   password = "password"
+#   User.create!(name: name, email: email, password: password, password_confirmation: password,activated: true, activated_at: Time.zone.now)
+# end
 
 
 training_type = ["Beacon & Recco", "Lift Evac", "Rope Rescue", "Avalanche Routes", "Funitel Evac", "Toboggan", "Dog Training", "Misc."]
@@ -18,11 +18,11 @@ training_type.each do |t|
   TrainingDivision.create!(training_type: t )
 end
 
-users = User.order(:created_at).take(6)
-10.times do
-  training_division = TrainingDivision.all.sample
-  location = Faker::Address.street_name
-  training_date = Faker::Date.backward(21)
-  trainer = Faker::Name.first_name
-  users.each { |user| user.trainings.create!(training_division_id: training_division.id, location: location, training_date: training_date, trainer: trainer) }
-end
+# users = User.order(:created_at).take(6)
+# 10.times do
+#   training_division = TrainingDivision.all.sample
+#   location = Faker::Address.street_name
+#   training_date = Faker::Date.backward(21)
+#   trainer = Faker::Name.first_name
+#   users.each { |user| user.trainings.create!(training_division_id: training_division.id, location: location, training_date: training_date, trainer: trainer) }
+# end
